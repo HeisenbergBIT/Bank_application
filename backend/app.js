83dd0115da495a2bde5bb8fa86652ca7eb6b4fa4
Mikołaj Grucel <73535344+HeisenbergBIT@users.noreply.github.com>
2023-09-03 18:16:04 +0200

    add restart server automatically

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middleware');
-    next();
-});
 
-app.use((req, res, next) => {
-    res.send('Hello');
+app.use("/api/posts",(req, res, next) => {
+    const posts = [
+        {id: "afesfdrg",
+        title: "cos tam",
+        content: "cos tam"
+    },
+    {id: "afesfefsfeg",
+        title: "cos tam2",
+        content: "cos tam2"
+    },
+    ];
+    res.status(200).json({
+        message: 'Posts',
+        posts: posts
+    });
 });
 
 module.exports = app;
