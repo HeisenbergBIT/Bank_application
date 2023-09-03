e8ef65849c1453ebb93e9ff840c9a94ff9b8e887
Mikołaj Grucel <73535344+HeisenbergBIT@users.noreply.github.com>
2023-09-03 22:39:33 +0200

    transform data

--- src/app/posts/post-service.ts
+++ src/app/posts/post-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post-model';
 
@@ -12,9 +13,20 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-    .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http
+    .get<{message: string, posts: any}>(
+        'http://localhost:3000/api/posts')
+    .pipe(map((postData) => {
+        return postData.posts.map(post => {
+            return {
+                title: post.title,
+                content: post.content,
+                id: post._id
+            };
+        });
+    }))
+    .subscribe(tranformedPosts => {
+        this.posts = tranformedPosts;
         this.postsUpdated.next([...this.posts]);
     })
   }
@@ -26,7 +38,8 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {id: null, title: title, content: content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http
+    .post<{message: string}>('http://localhost:3000/api/posts', post)
     .subscribe( responseData  => {
         console.log(responseData?.message);
         this.posts.push(post);
