6c02db89c8309326c9d2bcfbb52487f16d82f66d
Mikołaj Grucel <73535344+HeisenbergBIT@users.noreply.github.com>
2023-09-05 00:25:08 +0200

    Authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,57 +3,68 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
-const user = require("../models/user");
 
 const router = express.Router();
 
-router.post("/signup", (req,res,next) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
+router.post("/signup", (req, res, next) => {
+    
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    
+    const user = new User({
+      email: req.body.email,
+      password: hash
+      
     });
-    user.save().then(result => {
+    user
+      .save()
+      .then(result => {
         res.status(201).json({
-            message: 'User created',
-            result: result
+          message: "User created!",
+          result: result
         });
-    })
-    .catch(err => {
+      })
+      .catch(err => {
         res.status(500).json({
-            error: err
+          error: err
         });
-    });
-    });
+      });
+  });
 });
 
-router.post("/login", (req,res,next) => {
-    User.findOne({email: req.body.email})
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
     .then(user => {
-        if(!user) {
-            return res.status(401).json({
-                message: "Auth failed"
-            });
-        }
-        return bcrypt.compare(req.body.password, user.password);
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
     })
     .then(result => {
-        if(!result) {
-            return res.status(401).json({
-                message: "Auth failed"
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed"
         });
-        }
-        const token = jwt.sign({email: user.email, userId: user._id}, 'secret',{expiresIn: "1h"});
-        res.status(200).json({
-            token: token
-        });
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "secret",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token,
+        expiresIn: 3600
+      });
     })
     .catch(err => {
-        return res.status(401).json({
-            message: "Auth failed"
+      return res.status(401).json({
+        message: "Auth failed"
+      });
     });
-});
 });
 
 module.exports = router;
+
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from "rxjs";
 
 import { Post } from "../post-model";
 import { PostsService } from "../post-service";
+import { AuthService } from "../../authorization/signup/auth.service";
 
 @Component({
   selector: "app-post-list",
@@ -17,19 +18,30 @@ export class PostListComponent implements OnInit, OnDestroy {
   postsPerPage = 2;
   currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
+  userIsAuthenticated = false;
   private postsSub: Subscription;
+  private authStatusSub: Subscription;
 
-  constructor(public postsService: PostsService) {}
+  constructor(
+    public postsService: PostsService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.isLoading = true;
     this.postsService.getPosts(this.postsPerPage, this.currentPage);
     this.postsSub = this.postsService
       .getPostUpdateListener()
-      .subscribe((postData: {posts: Post[], postCount: number}) => {
+      .subscribe((postData: { posts: Post[]; postCount: number }) => {
         this.isLoading = false;
         this.totalPosts = postData.postCount;
         this.posts = postData.posts;
+      });
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
       });
   }
 
@@ -49,5 +61,6 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.postsSub.unsubscribe();
+    this.authStatusSub.unsubscribe();
   }
 }
